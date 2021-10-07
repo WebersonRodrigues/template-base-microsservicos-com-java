b0324ec2fff63d5b7430773d3929be3ed7ae06c8
Weberson.dsn.erp <weberson.dsn.erp@alterdata.com.br>
2021-10-06 21:00:56 -0300

    Subindo projeto inicial para base de uma arquitetura de microsserviços com java

---
+++ eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
